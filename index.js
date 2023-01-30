67c8d1588af25db66dbeae62fb3fa5854c0915b9
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-01-30 11:05:25 -0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const mainContaner = document.querySelector(".main__container");
+const thanksContainer = document.querySelector(".thank-you");
+const submitButton = document.getElementById("submit");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+
+submitButton.addEventListener("click",() =>{
+   thanksContainer.classList.remove("hidden")
+   mainContaner.style.display = "none" 
+})
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML++
+    })
+ })
